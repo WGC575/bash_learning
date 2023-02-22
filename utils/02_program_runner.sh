8fed5b34f2d67af7f3892dea220ce219a388502a
J.H <wgc575@gmail.com>
2023-02-21 20:02:40 -0800

    [v202302212002] Lua runner.

---
+++ utils/02_program_runner.sh
@@ -0,0 +1,6 @@
+#!bin/bash/
+
+# lua script environment path and the input script path
+./environment/lua-5.4.4/src/lua $1
+
+# in this way, one can run lua script without setting environment variables
